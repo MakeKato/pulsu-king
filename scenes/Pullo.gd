7badd1708f16163fbcd0cd5eecb537192f51584f
JenniMal <malmi.jenni@gmail.com>
2025-01-23 14:54:19 +0200

    Jenni

--- scenes/Pullo.gd
+++ scenes/Pullo.gd
@@ -6,7 +6,7 @@ func _ready():
 	connect("body_entered", self, "_on_body_entered")
 
 func _on_body_entered(body):
-	if body.is_in_group("Player"):  
+	if body.is_in_group("KinematicBody2D"):  
 		emit_signal("collected")   
 		queue_free()               
 
--- water.gd
+++ water.gd
@@ -1,9 +1,10 @@
 extends Area2D
 
-
 func _on_Area2D_body_entered(body):
-	if body.is_in_group("player"):
-	  
+	if body.name == "KinematicBody2D": 
+		print("Game Over: You tried to cross the river!")
+		# Esimerkki Game Over -toiminnosta:
+		get_tree().reload_current_scene()  
+func _on_area2d_body_entered(body):
+	if body == "KinematicBody2D":  
 		print("Game Over!")
-		
-		get_tree().paused = true  
