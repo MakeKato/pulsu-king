a0ecb306ff6aba0ea248d2f7fe66345ca616e426
JenniMal <malmi.jenni@gmail.com>
2025-02-05 11:45:43 +0200

    Jenni
    watereffect, collisionshape added etc

--- teeplayer.gd
+++ teeplayer.gd
@@ -6,3 +6,7 @@ func _ready():
 	var player = player_scene.instance()
 	player.position = GameManager.spawn_position
 	add_child(player)
+
+
+func _on_Area2D2_body_entered(body):
+	pass # Replace with function body.
