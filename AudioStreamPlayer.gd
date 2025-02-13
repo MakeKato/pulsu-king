19d4b698fca42c152ba17932758626bc8b297fa1
JenniMal <malmi.jenni@gmail.com>
2025-02-13 08:34:00 +0200

    audio code päivitys

--- AudioStreamPlayer.gd
+++ AudioStreamPlayer.gd
@@ -4,19 +4,16 @@ extends AudioStreamPlayer
 var music_player
 
 func _ready():
-	music_player = get_node("/root/Control/Menumusic")
-	music_player = get_node("/root/Control/Gameover")
-	
-	if music_player != null:
-		print("Menumusic node found!")
-	else:
-		print("Menumusic node NOT found!")
+	if has_node("Menumusic"):
+		music_player = get_node("Menumusic")
+	elif has_node("Gameover"):
+		music_player = get_node("Gameover")
 		
 func on_start_button_pressed():
-	if music_player != null:  
-		music_player.stop()  
+	if music_player != null:
+		music_player.stop()
 		
-		print("Control node: ", $Control)
+		print("Control node: ", $Control)  
 		print("Menumusic node: ", $Menumusic)
 	
 	get_tree().change_scene("res://World0.tscn")
