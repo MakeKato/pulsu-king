b2ffb703d6f8a496bf7f1bbf9d3f1a2ab8b29906
JenniMal <malmi.jenni@gmail.com>
2025-02-25 15:14:14 +0200

    Clock fix

--- teeplayer.gd
+++ teeplayer.gd
@@ -2,11 +2,15 @@ extends Node2D
 
 export (PackedScene) var player_scene
 
+onready var house = $House
+var player = null
+
 func _ready():
 	# Instantiate and set up the player
 	var player = player_scene.instance()
 	player.position = GameManager.spawn_position
 	add_child(player)
+
 
 	# Play the game music
 	var game_music = preload("res://Long Away Home.wav")
