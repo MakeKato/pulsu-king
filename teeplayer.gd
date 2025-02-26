a995bc78cd9855ed1543addaeec0eef89a23479b
JenniMal <malmi.jenni@gmail.com>
2025-02-26 18:50:37 +0200

    debugit korjattu

--- teeplayer.gd
+++ teeplayer.gd
@@ -7,9 +7,9 @@ func _ready():
 	player.position = GameManager.spawn_position
 	add_child(player)
 	
-	
-	var house_node = $House
-	player.set_house_node(house_node)
-
+	var house_node = get_node_or_null("House")
+	if house_node:
+		player.set_house_node(house_node)
+		
 	var game_music = preload("res://Long Away Home.wav")
 	Audiomanager.play_music(game_music)
